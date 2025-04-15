59a6244b10d935c70e32309111ed31cec4e1b188
Padmaja-30 <padmajaubbana30@gmail.com>
2025-04-15 17:48:59 +0530

    Add files via upload

---
+++ scripts/Courses.js
@@ -0,0 +1,11 @@
+// script.js (optional)
+document.querySelector('.btn-fill.small').addEventListener('click', () => {
+    alert('Search functionality can be added with JavaScript!');
+  });
+
+  
+  document.querySelector('.lang-btn').addEventListener('click', () => {
+    alert("Language selector coming soon!");
+  });
+
+  document.querySelector('.badge').textContent = 7;
